6ce70bb4e56fdea624d18824a29e2826deddd94b
xgf <xiong.guofeng@ocloudware.com>
2022-08-01 15:22:00 +0800

    first commit

---
+++ src/Views/AddItemView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Todo.Views
+{
+    public partial class AddItemView : UserControl
+    {
+        public AddItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
